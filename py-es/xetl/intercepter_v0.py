26938fa9f8c89da43bd11302017c9e82ff7665e7
admin <meigea@meigea.org>
2019-10-01 22:46:39 +0800

    :tada:---

--- py-es/xetl/intercepter_v0.py
+++ py-es/xetl/intercepter_v0.py
@@ -8,11 +8,18 @@ class Py3Destination4Es(object):
         self.outfile = open("/spool/log/example.txt", "a")
         # for key,v in msg.items():
         #     self.outfile.write(str(key) + " = " + str(v) + "\n");
+
+        full_item = msg['LEGACY_MSGHDR'] + msg['MESSAGE']
+        self.outfile.write(str(full_item))
+        # self.outfile.write( str(json.loads(str(full_item)) ))
+        self.outfile.write('==kkc=\n\n=====')
+
         for k, v in msg.items():
             self.outfile.write(str(k) + "\n")
             self.outfile.write(str(v)+"[{}]".format(str(type(v))) + "\n")
             self.outfile.write("___________侧_______EBD______\n\n")
-            
+        
+
         self.outfile.flush()
         self.outfile.close()
         
